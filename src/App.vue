6c43be5408d30f5c2f338e24fdfc00d880929370
Takáts Kristóf <takats.kristof.laszlo@petrik.hu>
2022-02-08 14:51:37 +0100

    listázás

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="statues">
+    <table>
+      <thead>
+        <tr>
+          <th>Azonsító</th>
+          <th>Person</th>
+          <th>Height</th>
+          <th>Price</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="statue in statues" v-bind:key="statue.id">
+          <td>{{ statue.id }}</td>
+          <td>{{ statue.person }}</td>
+          <td>{{ statue.height }}</td>
+          <td>{{ statue.price }}</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
+  name: 'Statues',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+        statue: {
+        id: null,
+        person: '',
+        height: '',
+        price: ''
+      },
+      statues: []
+    }
+  },
+  methods: {
+    async loadData () {
+     let Response = await fetch('http://127.0.0.1:8000/api/statues')
+     let data = await Response.json()
+     this.statues = data
+    },
+  }, mounted() {
+    this.loadData()
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
